ee77b7b765bf40cb5c22ef9627d569e37d861608
Rob Stenson <rob.stenson@gmail.com>
2013-12-03 22:38:44 -0500

    first build

--- build.js
+++ build.js
@@ -1,1 +1,7 @@
-build.js
+var hogan = require("hogan.js");
+var fs = require("fs");
+
+fs.readdir("images/", function(err, files) {
+  var index = hogan.compile(fs.readFileSync("index.hjs", { encoding:"utf8" }));
+  fs.writeFile("index.html", index.render({ images: files }, {}));
+});
