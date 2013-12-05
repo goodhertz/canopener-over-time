887c7d7ef8acafcff32bca85d456f3dc9ec37d33
Rob Stenson <rob.stenson@gmail.com>
2013-12-05 12:34:54 -0500

    preload

--- range.js
+++ range.js
@@ -1,4 +1,11 @@
 
+var loadedImages = [];
+for (var i = 0; i < images.length; i++) {
+  var image = new Image();
+  image.src = images[i];
+  loadedImages[i] = image;
+}
+
 $("#range").change(function() {
   var $visible = $(".screen img.visible");
   var $notVisible = $(".screen img").not(".visible");
