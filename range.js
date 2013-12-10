64238a9378239ee598b78215d7d67e09847079e9
Rob Stenson <rob.stenson@gmail.com>
2013-12-10 15:02:59 -0500

    prev/next

--- range.js
+++ range.js
@@ -9,7 +9,32 @@ for (var i = 0; i < images.length; i++) {
 $("#range").change(function() {
   var $visible = $(".screen img.visible");
   var $notVisible = $(".screen img").not(".visible");
-  console.log($visible.get(0), $notVisible.get(0));
   $notVisible.attr("src", images[parseInt(this.valueAsNumber, 10)]).addClass("visible");
   $visible.removeClass("visible");
+});
+
+var current = 0;
+var cycle = function() {
+    var $visible = $(".screen img.visible");
+    var $notVisible = $(".screen img").not(".visible");
+    $notVisible.attr("src", images[current]).addClass("visible").animate({ opacity: 1 });
+    $visible.removeClass("visible").animate({ opacity: 0 });
+};
+
+$("#prev").click(function() {
+  if (current > 0) {
+    current -= 1;
+  } else {
+    current = images.length - 1;
+  }
+  cycle();
+});
+
+$("#next").click(function() {
+  if (current < images.length - 1) {
+    current += 1;
+  } else {
+    current = 0;
+  }
+  cycle();
 });
